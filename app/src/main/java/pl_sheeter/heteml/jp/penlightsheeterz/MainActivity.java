28840165c1284337c963cd676a6058cf16629786
unionsep <union.septem@gmail.com>
2017-08-28 16:32:22 +0900

    add make penlight sheet webview

--- app/src/main/java/pl_sheeter/heteml/jp/penlightsheeterz/MainActivity.java
+++ app/src/main/java/pl_sheeter/heteml/jp/penlightsheeterz/MainActivity.java
@@ -2,6 +2,9 @@ package pl_sheeter.heteml.jp.penlightsheeterz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView psWebView = (WebView) findViewById(R.id.webview_penlight_sheeter);
+
+        psWebView.setWebChromeClient(new WebChromeClient());
+
+        WebSettings settings = psWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+
+        psWebView.loadUrl("http://souseiji.heteml.jp/kingbz/");
     }
 }
